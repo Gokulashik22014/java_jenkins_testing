5e8ab24bb19b0db51a6a4c6da7b1bd0ee445fb99
Gokulashik M <119474711+Gokulashik22014@users.noreply.github.com>
2025-09-18 12:09:42 +0530

    initial commit

---
+++ src/main/java/com/testing/javajenkinstest/JavajenkinstestApplication.java
@@ -0,0 +1,13 @@
+package com.testing.javajenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavajenkinstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavajenkinstestApplication.class, args);
+	}
+
+}
